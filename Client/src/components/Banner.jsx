09c7480aa2313c25450a6dc34df79be7a18ad548
afsalshamsudeen <itsmeafsalshamsudeen@gmail.com>
2025-09-15 14:00:03 +0000

    applyed mediaquery for banner

--- Client/src/components/Banner.jsx
+++ Client/src/components/Banner.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Banner = () => {
   return (
-    <div className='flex flex-col items-center justify-center text-slate-50 mt-4'>
-      <h1 className='text-center text-4xl font-medium'>Kickstart Your App<br/>in Seconds</h1>
-      <p className='m-0'>Choose your stack. Click generate. Start building.</p>
+    <div className='flex flex-col items-center justify-center text-slate-50 mt-2 md:mt-4'>
+      <h1 className='text-center text-2x1 font-medium md:text-4xl md:font-medium '>Kickstart Your App<br/>in Seconds</h1>
+      <p className='m-2 text-center text-sm md:text-2xl'>Choose your stack. Click generate. Start building.</p>
     </div>
   )
 }
