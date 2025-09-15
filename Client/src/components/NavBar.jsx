074fca85a117d353af507ff6be1360147fe5a4b9
afsalshamsudeen <itsmeafsalshamsudeen@gmail.com>
2025-09-15 13:47:07 +0000

    applyed media query-navbar

--- Client/src/components/NavBar.jsx
+++ Client/src/components/NavBar.jsx
@@ -1,39 +1,14 @@
-import React from 'react'
-import styled from 'styled-components';
+
 import logo from "../assets/layers.png";
 
-const Container = styled.div`
-   position: sticky;
-   top: 0;
-   z-index: 100;
-`;
-const LogoContainer = styled.div`
-    display: flex;
-    flex: 1;
-    align-items: center;
-    justify-content: flex-start;
-    width: 100%;
-    max-width: 1200px;
-    margin: 0 auto;
-    padding: 10px 80px;
-    color: aliceblue;
-    gap: 10px;
-    cursor: pointer;
-
-    
-`;
-const Logo = styled.img`
-    height: 30px;
-    width: 30px;
-`;
 const NavBar = () => {
   return (
-    <Container>
-      <LogoContainer>
-        <Logo src={logo}/>
+    <div className='sticky top-0 z-[100]'>
+      <div className='flex flex-1 items-center justify-center w-full m-0 auto px-10 py-4 text-white gap-3 cursor-pointer md:justify-start '>
+        <img className='h-4 w-auto md:h-7 md:w-auto' src={logo}/>
         <h2 className='text-lg font-poppins font-bold'>Stack Smith</h2>
-      </LogoContainer>
-    </Container>
+      </div>
+    </div>
   )
 }
 
